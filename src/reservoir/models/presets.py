a312162697ae490a62120ba60d5e0fdf611509fe
yoshi <yossyf1147@gmail.com>
2026-03-03 23:04:00 +0100

    fixed distillation

--- src/reservoir/models/presets.py
+++ src/reservoir/models/presets.py
@@ -103,31 +103,31 @@ DEFAULT_FNN_READOUT = FNNReadoutConfig(hidden_layers=(100,))
 "=============================================Classification Presets============================================"
 
 MINMAX_MNIST = MinMaxScalerConfig(
-    feature_min=-0.7675280665952444, #100
-    # feature_min=-0.6754946253854848,  # 1200
-    feature_max=0.35849784076318864, #100
-    # feature_max=0.8288112006441126,  # 1200
+    # feature_min=-0.7675280665952444, #100
+    feature_min=-0.6754946253854848,  # 1200
+    # feature_max=0.35849784076318864, #100
+    feature_max=0.8288112006441126,  # 1200
 )
 
 RP_MNIST = RandomProjectionConfig(
-    n_units=100,
-    input_scale=0.3543930218531782, #100
-    # input_scale=0.3478958243673553,  # 1200
-    input_connectivity=0.21745075681282766, #100
-    # input_connectivity=0.32024990697532185, # 1200
-    bias_scale=0.1725142451754484, #100
-    # bias_scale= 0.9911807193106197, # 1200
+    n_units=1200,
+    # input_scale=0.3543930218531782, #100
+    input_scale=0.3478958243673553,  # 1200
+    # input_connectivity=0.21745075681282766, #100
+    input_connectivity=0.32024990697532185, # 1200
+    # bias_scale=0.1725142451754484, #100
+    bias_scale= 0.9911807193106197, # 1200
     seed=1,
 )
 
 
 CLASSICAL_RESERVOIR_DYNAMICS = ClassicalReservoirConfig(
-    spectral_radius= 1.921291918880454, #100
-    # spectral_radius= 1.4707341636189577,  # 1200
-    leak_rate= 0.36449529864842045, #100
-    # leak_rate= 0.5078438853580478, #1200
-    rc_connectivity=0.6784641706491135, #100
-    # rc_connectivity=0.0760855941265183,  # 1200
+    # spectral_radius= 1.921291918880454, #100
+    spectral_radius= 1.4707341636189577,  # 1200
+    # leak_rate= 0.36449529864842045, #100
+    leak_rate= 0.5078438853580478, #1200
+    # rc_connectivity=0.6784641706491135, #100
+    rc_connectivity=0.0760855941265183,  # 1200
 
     seed=42,
     aggregation=AggregationMode.MEAN,
